bc7acfff85902ece282689c8a0016c6dbfce8a5f
alexander <world.kor.films@gmail.com>
2022-07-11 19:38:07 +0300

    delete todo item by touch Done

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react"
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from "./components/header"
+import TodoItem from "./components/todosItem";
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -9,15 +10,22 @@ export default function App() {
     { text: 'play on the switch', key: '3' }
   ]);
 
+  const pressHandler = (key) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.key !== key);
+    });
+  };
+
   return (
       <View style={styles.container}>
         <Header />
         <View style={styles.content}>
           <View style={styles.list}>
+
             <FlatList
                 data={todos}
                 renderItem={({ item }) => (
-                    <Text>{item.text}</Text>
+                    <TodoItem item={item} pressHandler={pressHandler} />
                 )}
             />
           </View>
